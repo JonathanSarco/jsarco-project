01de7f8493cd78848371e58ab722f381468c8316
Jony Sarco <jsarco@beerealit.com>
2020-06-30 15:28:10 -0300

    Add new page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,32 +1,31 @@
 import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
-import { Section, BoxContainer } from './styles';
-import { useWindowsDimensions } from '../hooks/useWindowsDimesions'
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { AnimatedHook } from '../animation/AnimatedHook';
+import { useWindowsDimensions } from '../hooks/useWindowsDimesions'
+import { Section, BoxContainer, ColBootstrap, WrapperDiv } from './styles';
 
 export const Home = () => {
     const { height } = useWindowsDimensions();
-    const [color, setColor] = useState('red') 
-
-    function changeColor() {
-        setColor('white'); 
-    }
+    const [color, setColor] = useState('red')
 
     return (
-        <Section id='home' alt='home' fullHeight={ height } selectedColor={ color }>
+        <Section id='home' alt='home' fullHeight={height} selectedColor={color}>
             <Container fluid>
-            <Row className="justify-content-center align-items-center">
-                <Col xs={2} />
-                <Col>
-                    <BoxContainer>
-
-                    </BoxContainer>
-                </Col>
-                <Col>
-                    <BoxContainer />
-                </Col>
-            </Row>
+                <Row className=''>
+                    <ColBootstrap xs={8} fullHeight={height} className='d-flex align-items-center'>
+                        <BoxContainer>
+                            <AnimatedHook />
+                        </BoxContainer>
+                    </ColBootstrap>
+                    <ColBootstrap fullHeight={height} className='d-flex align-items-center justify-content-center'>
+                        <WrapperDiv className='text-center'>
+                            <h1>Hi!</h1>
+                            <h2>I'm Jonathan</h2>
+                            <p className='main-text'>I am just testing React :)</p>
+                        </WrapperDiv>
+                    </ColBootstrap>
+                </Row>
             </Container>
         </Section>
     )
--- src/components/home/styles.js
+++ src/components/home/styles.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components'
 import backgroundImage from '../../images/home.jpg';
 import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
 
-export const Section = styled.section`
-    background-image: url(${backgroundImage});
+export const Section = styled.section` 
+    background-image: url(${backgroundImage});   
     height: ${props => props.fullHeight}px;
     background-color: ${props => props.selectedColor }
     width: 100%;
@@ -19,7 +20,37 @@ export const Section = styled.section`
 `;
 
 export const BoxContainer = styled(Container)`
-    border: 0px solid white;
-    width: 100%;
-    height: 891px;
+    border: 1px solid white;
+    align-items: center;
+    width: 50%
+`;
+
+export const ColBootstrap = styled(Col)`
+    height: ${props => props.fullHeight}px;
+    border: 1px white solid; 
+`;
+
+export const WrapperDiv = styled.div`
+    height: 50%;
+
+    h1 {
+        font-family: 'Noto Sans';
+        font-size: 5vw;
+        font-weight: 700;
+        font-style: bold;
+        color: #F0F8FF;
+    }
+
+    h2 {
+        font-family: 'Noto Sans';
+        font-size: 3vw;
+        font-style: bold;
+        color: #F0F8FF;
+    }
+
+    .main-text {
+        font-family: 'Noto Sans';
+        font-size: 2vw;
+        color: #F0F8FF;
+    }
 `;
