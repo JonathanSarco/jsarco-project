3bc094c02d8200f5d2572d9b202e6087b0f82515
Jonathan Sarco <jfe.sarco@gmail.com>
2020-08-16 19:51:06 -0300

    Add organism

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,68 +1,12 @@
 import React from 'react';
 import { Composition } from 'atomic-layout';
-import { defaultTemplate, mobileTemplate, formTemplate } from './templates';
-import { Form, Col, Button } from 'react-bootstrap'
+import { defaultTemplate, mobileTemplate } from './templates';
 import { useWindowsDimensions } from '../Hooks/useWindowsDimesions'
-import { 
-    Section, 
-    CardBox, 
-    Title, 
-    DivTitle, 
-    Paragraph, 
-    BLine, 
-    TextContainer, 
-    ButtonContainer,
-    MessageBox, 
-} from './styles';
+import { Section } from './styles';
+import { OForm } from './organism/OForm';
 
 export const Contact = () => {
     const { height } = useWindowsDimensions();
-
-    const CardLink = () => (
-        <Composition
-            areas={formTemplate}
-            gap={10}
-            gutter={50}
-            justifyContent='center'>
-            {
-                ({ MainTitle, FName, Message, SubmitArea }) => (
-                    <CardBox>
-                        <MainTitle flex flexDirection='column' alignItems='center'>
-                            <DivTitle>
-                                <Title color='rgb(225, 106, 106)'>Contact&nbsp;</Title>
-                                <Title color='white'>me</Title>
-                            </DivTitle>
-                            <Paragraph> If you want to ask me something, feel free to get in touch! </Paragraph>
-                            <BLine />
-                        </MainTitle>
-                        <Form>
-                            <FName>
-                                <Form.Row>
-                                    <Col>
-                                        <MessageBox placeholder='Name' />
-                                    </Col>
-                                    <Col>
-                                        <MessageBox type='email' placeholder='Email' />
-                                    </Col>
-                                </Form.Row>
-                            </FName>
-                            <Message flex flexDirection='column' alignItems='center'>
-                                <TextContainer>
-                                    <MessageBox as='textarea' rows='4' placeholder='Write me a cool (or not so cool) message...'/>
-                                </TextContainer>
-                            </Message>
-                        </Form>
-                        <SubmitArea flex flexDirection='column'>    
-                            <ButtonContainer >
-                                <Button variant='outline-secondary'>Send</Button>{' '}
-                            </ButtonContainer>
-                        </SubmitArea>
-                    </CardBox>
-                )
-            }
-        </Composition>
-
-    )
 
     return (
         <Section id='contact' alt='Contact me' fullHeight={height}>
@@ -73,13 +17,13 @@ export const Contact = () => {
                             flex
                             flexDirection='column'
                             justifyContent='space-between'>
-                            <CardLink />
+                            <OForm />
                         </Contact>
                         <Networks
                             flex
                             flexDirection='column'
                             justifyContent='space-between'>
-                            <CardLink />
+                            <OForm />
                         </Networks>
                     </>
                 )
---
+++ src/components/Contact/organism/OForm.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Composition } from 'atomic-layout';
+import { formTemplate } from '../templates';
+import { Form, Col, Button } from 'react-bootstrap'
+import { 
+    CardBox, 
+    Title, 
+    DivTitle, 
+    Paragraph, 
+    BLine, 
+    TextContainer, 
+    ButtonContainer,
+    MessageBox, 
+} from '../styles';
+
+export const OForm = () =>
+    (
+        <Composition
+            areas={formTemplate}
+            gap={10}
+            gutter={50}
+            justifyContent='center'>
+            {
+                ({ MainTitle, FName, Message, SubmitArea }) => (
+                    <CardBox>
+                        <MainTitle flex flexDirection='column' alignItems='center'>
+                            <DivTitle>
+                                <Title color='rgb(225, 106, 106)'>Contact&nbsp;</Title>
+                                <Title color='white'>me</Title>
+                            </DivTitle>
+                            <Paragraph> If you want to ask me something, feel free to get in touch! </Paragraph>
+                            <BLine />
+                        </MainTitle>
+                        <Form>
+                            <FName>
+                                <Form.Row>
+                                    <Col>
+                                        <MessageBox placeholder='Name' />
+                                    </Col>
+                                    <Col>
+                                        <MessageBox type='email' placeholder='Email' />
+                                    </Col>
+                                </Form.Row>
+                            </FName>
+                            <Message flex flexDirection='column' alignItems='center'>
+                                <TextContainer>
+                                    <MessageBox as='textarea' rows='4' placeholder='Write me a cool (or not so cool) message...' />
+                                </TextContainer>
+                            </Message>
+                        </Form>
+                        <SubmitArea flex flexDirection='column'>
+                            <ButtonContainer >
+                                <Button variant='outline-secondary'>Send</Button>{' '}
+                            </ButtonContainer>
+                        </SubmitArea>
+                    </CardBox>
+                )
+            }
+        </Composition>
+    );
